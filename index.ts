b90b9e6ea253a5f8cfe5ada660e599639cced9fc
Phạm Quang Trường <truongpham090325@gmail.com>
2025-12-07 08:55:04 +0700

    2.2. Xây dựng hệ thống lưu trữ file độc lập - Khởi tạo dự án

---
+++ index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = 4000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Website đang chạy trên cổng ${port}`);
+});
