b5cc3314cdfef055f10a0b93c26bba2ccc3008c3
Phạm Quang Trường <truongpham090325@gmail.com>
2025-12-10 09:56:21 +0700

    1.4. Tính năng Download file - FM

--- controllers/media.controller.ts
+++ controllers/media.controller.ts
@@ -3,9 +3,14 @@ import path from "path";
 
 export const getFile = (req: Request, res: Response) => {
   const filename = req.params.filename;
+  const type = req.query.type;
 
   // Đường dẫn đến file
   const mediaPath = path.join(__dirname, "../media", filename);
 
-  res.sendFile(mediaPath);
+  if (type == "download") {
+    res.download(mediaPath);
+  } else {
+    res.sendFile(mediaPath);
+  }
 };
